d8f557364804803c5b55319aef5f94318103556b
Jose Julio <jjpeajar@epsg.upv.es>
2022-11-07 17:06:49 +0100

    kost kon k

--- Assets/Scripts/canvasLogica.cs
+++ Assets/Scripts/canvasLogica.cs
@@ -19,4 +19,9 @@ public class canvasLogica : MonoBehaviour
     {
         SceneManager.LoadScene("InstruccionesScene");
     }
+
+    public void home()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
 }
